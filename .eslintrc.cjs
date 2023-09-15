e5d48310240051ebe8d103266bd89713d75d47f1
Kirill Sedyshev <agcrinax@gmail.com>
2023-09-15 13:23:24 +0300

    chore: recreate app because i cannot set up eslint :(

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,52 +1,19 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
-  },
+  root: true,
   'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-essential',
-    'prettier'
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
-  'overrides': [
-    {
-      'env': {
-        'node': true
-      },
-      'files': [
-        '.eslintrc.{js,cjs}'
-      ],
-      'parserOptions': {
-        'sourceType': 'script'
-      }
-    }
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'parser': '@typescript-eslint/parser',
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 'latest'
   },
-  'plugins': [
-    '@typescript-eslint',
-    'vue'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
+  rules: {
+    semi: [2, 'always'],
+    quotes: ['error', 'single']
   }
 };
