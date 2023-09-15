647494988e114e75f6f0bd01f02977d9332c7d81
Kirill Sedyshev <agcrinax@gmail.com>
2023-09-15 17:56:17 +0300

    feat(more): add more colors and buttons with colors in components

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,9 +1,75 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 
+export interface AppButtonProps {
+  kind?: 'primary' | 'success' | 'accent' | 'error' | 'info';
+}
+
+export interface AppButtonSlots {
+  icon?(): any,
+}
+
+const props = withDefaults(defineProps<AppButtonProps>(), { kind: 'primary' });
+const buttonClasses = computed(() => {
+  if (props.kind === 'primary') {
+    return ['app-button'];
+  }
+
+  return ['app-button', `app-button__${props.kind}`];
+});
+const slots = defineSlots<AppButtonSlots>();
 </script>
 
 <template>
-  <button>
-    <slot />
-  </button>
+  <div :class="buttonClasses" role="button">
+    <div class="app-button-icon" v-if="slots.icon">
+      <slot name="icon" />
+    </div>
+    <div class="app-button-text">
+      <slot />
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.app-button {
+  width: max-content;
+  padding: 8px 12px;
+  border-radius: 8px;
+  background-color: var(--color__primary);
+  box-shadow: 0px 0px 6px 0px #00000075;
+  display: flex;
+  gap: 4px;
+  font-size: 16px;
+  transition: .2s;
+
+  &-icon {
+    width: 12px;
+    height: 12px;
+  }
+
+  &:active {
+    box-shadow: 0px 0px 0px 0px #00000075
+  }
+
+  &:hover {
+    filter: brightness(120%);
+  }
+
+  &__success {
+    background-color: var(--color__success);
+  }
+
+  &__accent {
+    background-color: var(--color__accent);
+  }
+
+  &__error {
+    background-color: var(--color__error);
+  }
+
+  &__info {
+    background-color: var(--color__info);
+  }
+}
+</style>
--- src/views/ComponentView.vue
+++ src/views/ComponentView.vue
@@ -4,5 +4,18 @@ import AppButton from '@/components/AppButton.vue';
 </script>
 
 <template>
-  <AppButton>Sample button</AppButton>
+  <div class="app-components buttons">
+    <AppButton kind="primary">Primary button</AppButton>
+    <AppButton kind="success">Success button</AppButton>
+    <AppButton kind="accent">Accent button</AppButton>
+    <AppButton kind="error">Error button</AppButton>
+    <AppButton kind="info">Info button</AppButton>
+  </div>
 </template>
+
+<style scoped>
+.app-components.buttons {
+  display: flex;
+  gap: 5px;
+}
+</style>
